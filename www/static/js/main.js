ded4dfbd4a764a2a65305025cf8fb05a82cb38eb
sirh3e <sirh3e@protonmail.com>
2024-03-04 22:14:03 +0100

    [#20]: WIP does not work add all just for the changes :D

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
+import * as wasm from "wasm-rubiks-cube-solver";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { initCube, animateCube, onMouseClick } from './controls.js';
+import {initSteps, convertToMove, convertMovesToSteps} from './steps.js';
 import config from '../config/config.json';
 
 const scene = new THREE.Scene();
@@ -10,21 +12,129 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({ antialias: true }); // Enable anti-aliasing
 const composer = new EffectComposer(renderer);
 const controls = new OrbitControls(camera, renderer.domElement);
-const cube = initCube(scene);
+let cube = initCube(scene);
+let steps_state = initSteps(convertMovesToSteps(["L", "R"]));
 
 // todo: assign actual functions once possible
-document.getElementById("start").addEventListener("click", () => { cube.rotateFace("f"); });
-document.getElementById("prev").addEventListener("click", () => { cube.rotateFace("B"); });
-document.getElementById("playPause").addEventListener("click", () => { cube.rotateFace("L"); });
-document.getElementById("next").addEventListener("click", () => { cube.rotateFace("r"); });
-document.getElementById("end").addEventListener("click", () => { cube.rotateFace("d'"); });
-document.getElementById("reset").addEventListener("click", () => { cube.rotateFace("u'"); });
+document.getElementById("start").addEventListener("click", () => {
+    let moves = null;
+    while ((moves = steps_state.undo()) != null) {
+        moves.forEach(move => cube.rotateFace(move));
+    }
+});
+document.getElementById("prev").addEventListener("click", () => {
+    let moves = steps_state.undo();
+    if (moves == null) {
+        return;
+    }
+    moves.forEach(move => cube.rotateFace(move));
+});
+document.getElementById("playPause").addEventListener("click", () => {
+    //ToDo add a pause function
+    let move = null;
+    while((move = steps_state.do()) != null){
+        cube.rotateFace(move);
+    }
+    const steps_span = document.getElementById('steps');
+    steps_state.steps.forEach(step => {
+        const step_span = document.createElement('span');
+        step_span.innerText = step.move;
+        steps_span.appendChild(step_span);
+    });
+    setActiveStep(steps_state);
+});
+document.getElementById("next").addEventListener("click", () => {
+    let move = steps_state.do();
+    if (move == null) {
+        return;
+    }
+    cube.rotateFace(move);
+});
+document.getElementById("end").addEventListener("click", () => {
+    let move = null;
+    while((move = steps_state.do()) != null){
+        cube.rotateFace(move);
+    }
+});
+document.getElementById("reset").addEventListener("click", () => {
+    scene = new THREE.Scene();
+    cube = initCube(scene);
+    steps_state = initSteps();
+});
 
+// B = green
+// D = orange
+// F = red
+// L = yellow
+// R = white
+// U = blue
 
 document.getElementById("show").addEventListener("click", () => {
     const cube_state = cube.getCubeState();
     console.log(cube_state);
+document.getElementById("solve").addEventListener("click", () => {
+    const cube_state = cube.getCubeState();
+    const names = Object.getOwnPropertyNames(cube_state);
+
+    const map = {}
+    names.map(name => cube_state[name].map(mapFrontend2Backend).join("")).forEach(face => {
+        map[face[4]] = face
+    });
+
+    const cube_str = map["W"] + map["R"] + map["G"] + map["Y"] + map["O"] + map["B"];
+    //console.log(JSON.stringify(cube_str));
+
+    const moves = wasm.solve_cube(cube_str);
+    //console.log(JSON.stringify(moves));
+
+    const steps = convertMovesToSteps(moves.map(convertToMove));
+    steps_state.setSteps(steps);
+
+    const steps_span = document.getElementById('steps');
+    steps_state.steps.forEach(step => {
+        const step_span = document.createElement('span');
+        step_span.innerText = step.move;
+        steps_span.appendChild(step_span);
+    });
+
+    setActiveStep(steps_state);
 });
+
+function setActiveStep(steps_state){
+    const steps_span = document.getElementById('steps');
+
+    const children = Array.from(steps_span.children);
+    children.forEach(child => child.classList.remove('step-active'));
+
+    console.log()
+
+    if (steps_span.children[steps_state.index]){
+        steps_span.children[steps_state.index].classList.add('step-active');
+    }
+}
+
+function mapFrontend2Backend(color){
+    if (color === "green") {
+        return "G"
+    }
+    if (color === "orange") {
+        return "O"
+    }
+    if (color === "red") {
+        return "R"
+    }
+    if (color === "yellow") {
+        return "Y"
+    }
+    if (color === "white") {
+        return "W"
+    }
+    if (color === "blue") {
+        return "B"
+    }
+    throw new Error("The color name: '" + color + "' is invalid")
+}
+
 
 
 function onWindowResize() {
